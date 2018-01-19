36c0dc4526d57dda384b47a7c8f2a4ffa48afb78
mmotti <35599110+mmotti@users.noreply.github.com>
2018-01-19 12:55:35 +0000

    Added support for wildcards

--- app/src/main/java/com/fiendfyre/AdHell2/blocker/ContentBlocker56.java
+++ app/src/main/java/com/fiendfyre/AdHell2/blocker/ContentBlocker56.java
@@ -12,10 +12,12 @@ import com.fiendfyre.AdHell2.db.entity.BlockUrl;
 import com.fiendfyre.AdHell2.db.entity.BlockUrlProvider;
 import com.fiendfyre.AdHell2.db.entity.UserBlockUrl;
 import com.fiendfyre.AdHell2.db.entity.WhiteUrl;
+import com.fiendfyre.AdHell2.utils.BlockUrlPatternsMatch;
 import com.sec.enterprise.AppIdentity;
 import com.sec.enterprise.firewall.DomainFilterRule;
 import com.sec.enterprise.firewall.Firewall;
 import com.sec.enterprise.firewall.FirewallResponse;
+import com.sec.enterprise.firewall.FirewallRule;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -58,18 +60,23 @@ public class ContentBlocker56 implements ContentBlocker {
         if (isEnabled()) {
             disableBlocker();
         }
+
+        /*
         BlockUrlProvider standardBlockUrlProvider =
                 appDatabase.blockUrlProviderDao().getByUrl(MainActivity.ADHELL_STANDARD_PACKAGE);
         List<BlockUrl> standardList = appDatabase.blockUrlDao().getUrlsByProviderId(standardBlockUrlProvider.id);
+        */
 
         Set<BlockUrl> finalBlockList = new HashSet<>();
-        finalBlockList.addAll(standardList);
+        //finalBlockList.addAll(standardList);
         List<BlockUrlProvider> blockUrlProviders = appDatabase.blockUrlProviderDao().getBlockUrlProviderBySelectedFlag(1);
 
         for (BlockUrlProvider blockUrlProvider : blockUrlProviders) {
+            /*
             if (blockUrlProvider.url.equals(MainActivity.ADHELL_STANDARD_PACKAGE)) {
                 continue;
             }
+            */
             Log.i(TAG, "Included url provider: " + blockUrlProvider.url);
             List<BlockUrl> blockUrls = appDatabase.blockUrlDao().getUrlsByProviderId(blockUrlProvider.id);
             if (finalBlockList.size() + blockUrls.size() <= this.urlBlockLimit - 100) {
@@ -92,12 +99,50 @@ public class ContentBlocker56 implements ContentBlocker {
 
         List<String> denyList = new ArrayList<>();
         for (BlockUrl blockUrl : finalBlockList) {
+            /*
             if (Patterns.WEB_URL.matcher(blockUrl.url).matches()) {
                 if (whiteUrlsString.contains(blockUrl.url)) {
                     continue;
                 }
                 denyList.add("*" + blockUrl.url + "*");
             }
+            */
+
+            if (whiteUrlsString.contains(blockUrl.url)) {
+                continue;
+            }
+
+            // If a wildcard entry is passed, bypass current URL filter
+            if(blockUrl.url.contains("*"))
+            {
+                Log.d(TAG, "Wildcard detected -->" + blockUrl.url + " requires validation.");
+
+                // Check the wildcard is valid
+                boolean validWildcard = BlockUrlPatternsMatch.wildcardValid(blockUrl.url);
+
+                // If it isn't valid, skip it
+                if(!validWildcard)
+                {
+                    Log.d(TAG, blockUrl.url + " is not a valid wildcard.");
+                    continue;
+                }
+
+                Log.d(TAG, "Wildcard verified.");
+
+                final String urlReady = blockUrl.url;
+
+                denyList.add(urlReady);
+            }
+            // If it a wildcard isn't detected, let's use AdHell's original processing.
+            else if (Patterns.WEB_URL.matcher(blockUrl.url).matches())
+            {
+                final String urlReady = "*" + blockUrl.url;
+
+                Log.d(TAG, "Adding rule: " + urlReady);
+
+                denyList.add(urlReady);
+            }
+
         }
 
         List<UserBlockUrl> userBlockUrls = appDatabase.userBlockUrlDao().getAll2();
@@ -105,10 +150,22 @@ public class ContentBlocker56 implements ContentBlocker {
         if (userBlockUrls != null && userBlockUrls.size() > 0) {
             Log.i(TAG, "UserBlockUrls size: " + userBlockUrls.size());
             for (UserBlockUrl userBlockUrl : userBlockUrls) {
+                /*
                 if (Patterns.WEB_URL.matcher(userBlockUrl.url).matches()) {
                     denyList.add("*" + userBlockUrl.url + "*");
                     Log.i(TAG, "UserBlockUrl: " + userBlockUrl.url);
                 }
+                */
+
+                if (Patterns.WEB_URL.matcher(userBlockUrl.url).matches()) {
+
+                    final String urlReady = "*" + userBlockUrl.url + "*";
+
+                    denyList.add(urlReady);
+
+                    Log.i(TAG, "UserBlockUrl: " + urlReady);
+                }
+
             }
         } else {
             Log.i(TAG, "UserBlockUrls is empty.");
@@ -127,8 +184,38 @@ public class ContentBlocker56 implements ContentBlocker {
             Log.d(TAG, app.packageName);
             rules.add(new DomainFilterRule(new AppIdentity(app.packageName, null), new ArrayList<>(), superAllow));
         }
+
         try {
+            /* Try to block Port 53 */
+
+            Log.d(TAG, "Adding: DENY PORT 53");
+
+            // Number of rules
+            int numRules = 2;
+            // Declare new firewall rule variable
+            FirewallRule[] portRules = new FirewallRule[numRules];
+
+            // Add deny rules for DNS port (53)
+            portRules[0] = new FirewallRule(FirewallRule.RuleType.DENY, Firewall.AddressType.IPV4);
+            portRules[0].setIpAddress("*");
+            portRules[0].setPortNumber("53");
+
+            portRules[1] = new FirewallRule(FirewallRule.RuleType.DENY, Firewall.AddressType.IPV6);
+            portRules[1].setIpAddress("*");
+            portRules[1].setPortNumber("53");
+
+            FirewallResponse[] response = mFirewall.addRules(portRules);
+        }
+        catch (SecurityException ex)
+        {
+            Log.e(TAG, "Failed to add PORT rule.", ex);
+            return false;
+        }
+
+        try {
+            Log.d(TAG, "Adding: DENY DOMAINS");
             FirewallResponse[] response = mFirewall.addDomainFilterRules(rules);
+
             if (!mFirewall.isFirewallEnabled()) {
                 mFirewall.enableFirewall(true);
             }
@@ -153,7 +240,12 @@ public class ContentBlocker56 implements ContentBlocker {
     public boolean disableBlocker() {
         FirewallResponse[] response;
         try {
+            // Clear IP rules
+            response = mFirewall.clearRules(Firewall.FIREWALL_ALL_RULES);
+
+            // Clear domain filter rules
             response = mFirewall.removeDomainFilterRules(DomainFilterRule.CLEAR_ALL);
+
             Log.i(TAG, "disableBlocker " + response[0].getMessage());
             if (mFirewall.isFirewallEnabled()) {
                 mFirewall.enableFirewall(false);
@@ -162,7 +254,7 @@ public class ContentBlocker56 implements ContentBlocker {
                 mFirewall.enableDomainFilterReport(false);
             }
         } catch (SecurityException ex) {
-            Log.e(TAG, "Failed to removeDomainFilterRules", ex);
+            Log.e(TAG, "Failed to remove firewall rules", ex);
             return false;
         }
         return true;
